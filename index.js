a545ae6db27d37827b3ee68fbf07e70f7280d394
Yoshiro <r.yoshiki0000@gmail.com>
2025-04-30 18:34:26 +0800

    added p2p fixes

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ wss.on('connection', (ws) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
